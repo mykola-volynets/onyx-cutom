7b83542dade298190f07944307e8455607695c57
Volyn3ts <volynetskolia@gmail.com>
2025-06-09 14:01:38 +0200

    hotfix

--- custom_extensions/frontend/src/components/PdfLessonDisplay.tsx
+++ custom_extensions/frontend/src/components/PdfLessonDisplay.tsx
@@ -570,7 +570,7 @@ const PdfLessonDisplay = ({ dataToDisplay, isEditing, onTextChange }: PdfLessonD
                     isLastInBox={isCurrentItemEffectivelyLast} 
                     isEditing={isEditing} onTextChange={onTextChange} 
                     basePath={['contentBlocks', findOriginalIndex(item.content)]}
-                    isLastSectionHeaderWithStar={isLastHeaderForStar && item.content.type === 'headline'}
+                    isLastSectionHeaderWithStar={isLastHeaderForStar && 'type' in item.content && item.content.type === 'headline'}
                 /> 
               )}
             </React.Fragment>
